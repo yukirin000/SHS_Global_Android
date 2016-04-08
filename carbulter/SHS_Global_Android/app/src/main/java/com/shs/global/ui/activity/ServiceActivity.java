e4976c1e1307613d42340412dd0cff90dc8ef163
yukirin002 <yukirin_002@163.com>
2016-04-08 19:26:26 +0800

    修改了部分UI

--- carbulter/SHS_Global_Android/app/src/main/java/com/shs/global/ui/activity/ServiceActivity.java
+++ carbulter/SHS_Global_Android/app/src/main/java/com/shs/global/ui/activity/ServiceActivity.java
@@ -1,11 +1,20 @@
 package com.shs.global.ui.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.shs.global.R;
 
 /**
  * Created by wenhai on 2016/4/7.
  */
 public class ServiceActivity extends BaseActivityWithTopBar{
+    @ViewInject(R.id.call_telephone)
+    private LinearLayout button;
+
     @Override
     public int setLayoutId() {
         return R.layout.activity_service;
@@ -14,5 +23,12 @@ public class ServiceActivity extends BaseActivityWithTopBar{
     @Override
     protected void setUpView() {
             setBarText("服务");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "4008693911"));
+                startActivity(intent);
+            }
+        });
     }
 }
